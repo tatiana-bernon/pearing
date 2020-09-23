32f4e4211d5d3a6583cf33aaa2d31da9a3818142
Tatiana Bernon <42634827+tatiana-bernon@users.noreply.github.com>
2020-09-24 10:53:50 +1200

    landingpage comp

---
+++ client/components/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+
+function Landing () {
+  return (
+    <>
+    <div className='landingPage'>
+      <h1> Welcome to Pearing! </h1>
+    </div>
+    </>
+  )
+}
+
+export default Landing
