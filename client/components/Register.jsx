29b5786fb959ec787c7f8fa54487d7b3c859dc60
Tatiana Bernon <42634827+tatiana-bernon@users.noreply.github.com>
2020-09-24 10:34:01 +1200

    Create register component

---
+++ client/components/Register.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useContext } from 'react'
+import { register, isAuthenticated } from 'authenticare/client'
+
+import { UserContext, updateUserContext } from './UserContext'
+
+function Register (props) {
+  const [, setUser] = useContext(UserContext)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const baseUrl = '/api/v1'
+
+  const handleClick = () => {
+    register(
+      {
+        username: username,
+        password: password
+      },
+      { baseUrl }
+    )
+      .then(() => {
+        if (isAuthenticated()) {
+          updateUserContext(setUser)
+          return props.history.push('/user')
+        }
+        return null
+      })
+      .catch((error) => {
+        console.log('error: ', error.message)
+      })
+  }
+
+  return (
+    <div className="registerContainer container">
+      <div className="leftRegister">
+        <div className="registerTitle">
+          <h1>Register</h1>
+        </div>
+        <label className="label">Username</label>
+        <input
+          className="input"
+          type="username"
+          value={username}
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+        <label className="label">Password</label>
+        <input
+          className="input"
+          type="password"
+          value={password}
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <label className="label">pearing</label>
+
+        <button
+          type="button"
+          className="button is-primary"
+          onClick={handleClick}
+          data-testid="submitButton"
+        >
+          Register
+        </button>
+      </div>
+      <div className="rightRegister">
+        <img
+          src="#"
+          alt=""
+          style={{ width: '600px', height: '500px' }}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Register
