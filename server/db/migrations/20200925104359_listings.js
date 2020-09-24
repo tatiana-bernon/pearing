4b2bcaac143cb8a5b9fb720568a43a6c48170d5e
Bhavisha Das <66805189+bhavishaDas@users.noreply.github.com>
2020-09-25 11:24:29 +1200

    createtable

--- server/db/migrations/20200925104359_listings.js
+++ server/db/migrations/20200925104359_listings.js
@@ -1,7 +1,12 @@
 exports.up = (knex) => {
   return knex.schema.createTable('listings', table => {
     table.increments('id')
-    table.string('listings')
+    table.string('title')
+    table.integer('subject_id')
+    table.string('description')
+    table.integer('counter')
+    table.integer('interested')
+    table.integer('user_id')
   })
 }
 
--- server/db/seeds/03_listings.js
+++ server/db/seeds/03_listings.js
@@ -5,10 +5,10 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('listings').insert([
-        {id: 1, title: 'Help needed with redux', subject: 'redux', description: 'placeholder', counter: '1', interested: '4', author: 'Bhiv'},
-        {id: 2, title: 'Looking to pair up yo learn hooks', subject: 'react hooks', description: 'placeholder', counter: '2', interested: '0', author: 'Tati'},
-        {id: 3, title: 'Needing some help with sass', subject: 'sass', description: 'placeholder', counter: '3', interested: '3', author: 'Andras'},
-        {id: 4, title: 'Back to basics. Need help with HTML',subject: 'HTML', description: 'placeholder', counter: '4', interested: '2', author: 'Daryl'}
+        {id: 1, title: 'Help needed with redux', subject_id: 'redux', description: 'placeholder', counter: '1', interested: '4', user_id: 'Bhiv'},
+        {id: 2, title: 'Looking to pair up yo learn hooks', subject_id: 'react hooks', description: 'placeholder', counter: '2', interested: '0', user_id: 'Tati'},
+        {id: 3, title: 'Needing some help with sass', subject_id: 'sass', description: 'placeholder', counter: '3', interested: '3', user_id: 'Andras'},
+        {id: 4, title: 'Back to basics. Need help with HTML',subject_id: 'HTML', description: 'placeholder', counter: '4', interested: '2', user_id: 'Daryl'}
       ]);
     });
 };
