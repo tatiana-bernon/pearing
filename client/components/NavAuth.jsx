b604a093696cc348dbd0a7659c8e13c1c27c3836
Andras Balint <anoctus@protonmail.com>
2020-09-25 10:24:57 +1200

    subject client route fixed in Nav

--- client/components/NavAuth.jsx
+++ client/components/NavAuth.jsx
@@ -114,7 +114,7 @@ export default function NavAuth(props) {
           <Link to="/about">About</Link>
           <Link to="/contact">Contact</Link>
           <Link to="/profile">Profile</Link>
-          <Link to="/subject">Subject</Link>
+          <Link to="/subjects">Subjects</Link>
         </>
       )
       break
@@ -123,7 +123,7 @@ export default function NavAuth(props) {
         <>
           <Link to="/about">About</Link>
           <Link to="/contact">Contact</Link>
-          <Link to="/subject">Subject</Link>
+          <Link to="/subjects">Subjects</Link>
           <Link to="/home">Home</Link>
         </>
       )
@@ -133,7 +133,7 @@ export default function NavAuth(props) {
         <>
           <Link to="/about">About</Link>
           <Link to="/profile">Profile</Link>
-          <Link to="/subject">Subject</Link>
+          <Link to="/subjects">Subjects</Link>
           <Link to="/home">Home</Link>
         </>
       )
@@ -143,10 +143,21 @@ export default function NavAuth(props) {
         <>
           <Link to="/contact">Contact</Link>
           <Link to="/profile">Profile</Link>
-          <Link to="/subject">Subject</Link>
+          <Link to="/subjects">Subjects</Link>
           <Link to="/home">Home</Link>
         </>
       )
+      break
+    case '/subjects':
+      navLanding = (
+        <>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/home">Home</Link>
+        </>
+      )
+      break  
   }
 
 
