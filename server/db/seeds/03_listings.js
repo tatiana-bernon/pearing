d6c7876668a799bdc0377f658e9fa83eb8541a55
Bhavisha Das <66805189+bhavishaDas@users.noreply.github.com>
2020-09-25 11:09:35 +1200

    added table

--- server/db/seeds/03_listings.js
+++ server/db/seeds/03_listings.js
@@ -1,13 +1,14 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('listings').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('listings').insert([
+        {id: 1, title: 'Help needed with redux', subject: 'redux', description: 'placeholder', counter: '1', interested: '4', author: 'Bhiv'},
+        {id: 2, title: 'Looking to pair up yo learn hooks', subject: 'react hooks', description: 'placeholder', counter: '2', interested: '0', author: 'Tati'},
+        {id: 3, title: 'Needing some help with sass', subject: 'sass', description: 'placeholder', counter: '3', interested: '3', author: 'Andras'},
+        {id: 4, title: 'Back to basics. Need help with HTML',subject: 'HTML', description: 'placeholder', counter: '4', interested: '2', author: 'Daryl'}
       ]);
     });
 };
