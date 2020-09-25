ff804f313e3419d26d0fb5f996687a7e34246983
Tatiana Bernon <42634827+tatiana-bernon@users.noreply.github.com>
2020-09-25 14:19:13 +1200

    Add getListingById route

--- server/routes/listings.js
+++ server/routes/listings.js
@@ -4,21 +4,21 @@ const db = require('../db/listings')
 
 const router = express.Router()
 
-// router.get('/:id', (req, res) => {
-//   const id = Number(req.params.id)
-//   db.getUserGarden(id)
-//     .then(singleGarden => {
-//       return res.json(singleGarden)
-//     })
-//     .catch(err => {
-//       res.status(500).json({ error: err.message })
-//     })
-// })
-
 router.get('/', (req, res) => {
   db.getList()
     .then(list => {
       return res.json({ list })
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message })
+    })
+})
+
+router.get('/:id', (req, res) => {
+  const id = Number(req.params.id)
+  db.getListingById(id)
+    .then(listing => {
+      return res.json(listing)
     })
     .catch(err => {
       res.status(500).json({ error: err.message })
