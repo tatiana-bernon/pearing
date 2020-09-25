1c9306ec4149346a9f5de7c8e6eb405c41008a4c
Daryl-creator <darylchen.w.h@gmail.com>
2020-09-26 11:57:33 +1200

    aligned component with repo

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,11 +1,9 @@
-/* eslint-disable indent */
-/* eslint-disable space-before-function-paren */
 import React, { useState, useContext } from 'react'
 import { register, isAuthenticated } from 'authenticare/client'
 
 import { UserContext, updateUserContext } from './UserContext'
 
-function Register(props) {
+function Register (props) {
   const [, setUser] = useContext(UserContext)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -32,40 +30,38 @@ function Register(props) {
   }
 
   return (
-    <>
-      <div className="registerContainer container">
-        <div className="leftRegister">
-          <div className="registerTitle">
-            <h1>Register</h1>
-          </div>
-
-          <label className="label">Username</label>
-          <input
-            className="input"
-            type="username"
-            value={username}
-            placeholder="username"
-            onChange={(e) => setUsername(e.target.value)}>
-          </input>
-          <label className="label">Password</label>
-          <input
-            className="input"
-            type="password"
-            value={password}
-            placeholder="password"
-            onChange={(e) => setPassword(e.target.value)}>
-          </input>
-          <button
-            type="button"
-            className="button is-primary"
-            onClick={handleClick}
-            data-testid="submitButton">
+    <div className="registerContainer container">
+      <div className="leftRegister">
+        <div className="registerTitle">
+          <h1>Register</h1>
+        </div>
+        <label className="label">Username</label>
+        <input
+          className="input"
+          type="username"
+          value={username}
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}>
+        </input>
+        <label className="label">Password</label>
+        <input
+          className="input"
+          type="password"
+          value={password}
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}>
+        </input>
+        <button
+          type="button"
+          className="button is-primary"
+          onClick={handleClick}
+          data-testid="submitButton">
           Register
-          </button>
+        </button>
       </div>
       <div className="rightRegister">
       </div>
-      </>
+    </div>
   )
 }
 
