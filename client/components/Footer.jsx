9df7f8ce2a0e827cb6702c9ffe37ab8ea3f69833
Tatiana Bernon <42634827+tatiana-bernon@users.noreply.github.com>
2020-09-27 12:10:18 +1300

    Solve conflicts with footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -8,11 +8,7 @@ const Footer = () => {
 
       <footer className="container">
 
-<<<<<<< HEAD
-        <div className="column">
-=======
         <div className="is-size-4  has-text-centered content has-text-centered mx-6 mt-6 mb-1">
->>>>>>> 0f13960c8f21b02c3dfd8685d601fe95c0e7c7c3
           <p><strong>pearing&#169; </strong> Pōhutukawa Cohort 2020</p>
         </div>
       </footer>
