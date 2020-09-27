1694faa11c0ce9a9dfc179477967b712e3bf9054
Tatiana Bernon <42634827+tatiana-bernon@users.noreply.github.com>
2020-09-28 11:31:14 +1300

    addstyling to contact

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -4,18 +4,18 @@ import React from 'react'
 function Contact () {
   return (
     <>
-     <h1 className="has-text-primary is-size-1 has-text-centered mx-6 mt-6 mb-6"> Send us a message! </h1>
+      <h1 className="has-text-primary is-size-1 has-text-centered mx-6 mt-6 mb-6"> Send us a message! </h1>
       <div className="field">
         <label className="label">Name</label>
         <div className="control">
-        <input className="input is-primary" type="text" placeholder="Primary input" />
+          <input className="input is-primary" type="text" placeholder="Primary input" />
         </div>
       </div>
 
       <div className="field">
         <label className="label">Username</label>
         <div className="control has-icons-left has-icons-right">
-          <input className="input is-success" type="text" placeholder="Text input" value="bulma" />
+          <input className="input is-primary" type="text" placeholder="Primary input" />
           <span className="icon is-small is-left">
             <i className="fas fa-user"></i>
           </span>
@@ -23,13 +23,13 @@ function Contact () {
             <i className="fas fa-check"></i>
           </span>
         </div>
-        <p className="help is-success">This username is available</p>
+        {/* <p className="help is-success">This username is available</p> */}
       </div>
 
       <div className="field">
         <label className="label">Email</label>
         <div className="control has-icons-left has-icons-right">
-          <input className="input is-danger" type="email" placeholder="Email input" value="hello@" />
+          <input className="input is-primary" type="text" placeholder="Primary input" />
           <span className="icon is-small is-left">
             <i className="fas fa-envelope"></i>
           </span>
@@ -37,7 +37,7 @@ function Contact () {
             <i className="fas fa-exclamation-triangle"></i>
           </span>
         </div>
-        <p className="help is-danger">This email is invalid</p>
+        {/* <p className="help is-danger">This email is invalid</p> */}
       </div>
 
       <div className="field">
@@ -55,29 +55,7 @@ function Contact () {
       <div className="field">
         <label className="label">Message</label>
         <div className="control">
-        <textarea className="textarea is-primary" placeholder="Primary textarea"></textarea>
-        </div>
-      </div>
-
-      <div className="field">
-        <div className="control">
-          <label className="checkbox">
-            <input type="checkbox" />
-                    I agree to the <a href="#">terms and conditions</a>
-          </label>
-        </div>
-      </div>
-
-      <div className="field">
-        <div className="control">
-          <label className="radio">
-            <input type="radio" name="question" />
-                      Yes
-    </label>
-          <label className="radio">
-            <input type="radio" name="question" />
-                        No
-          </label>
+          <textarea className="textarea is-primary" placeholder="Primary textarea"></textarea>
         </div>
       </div>
 
@@ -86,7 +64,7 @@ function Contact () {
           <button className="button is-primary">Submit</button>
         </div>
         <div className="control">
-          <button className="button is-primary is-light">Cancel</button>
+          <button className="button is-primary is-outlined">Cancel</button>
         </div>
       </div>
     </>
