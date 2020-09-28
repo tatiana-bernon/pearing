ea5f718af33ef13ffe78d859e1cf8d5279e30f34
Tatiana Bernon <42634827+tatiana-bernon@users.noreply.github.com>
2020-09-28 19:25:51 +1300

    playing with navbar

--- client/components/NavAuth.jsx
+++ client/components/NavAuth.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { logOff } from 'authenticare/client'
 import { IfAuthenticated } from './Authenticated'
 
-export default function NavAuth(props) {
+export default function NavAuth (props) {
   const currentPage = props.location.pathname
   let navLanding = null
 
@@ -22,22 +22,6 @@ export default function NavAuth(props) {
               <a className="navbar-item">
                 <Link to="/profile">Profile</Link>
               </a>
-            </div>
-          </div>
-        </>
-      )
-      break
-    case '/profile':
-      navLanding = (
-        <>
-          <div id="navbarBasicExample " className="navbar-menu is-size-3">
-            <div className="navbar-end">
-              <a className="navbar-item">
-                <Link to="/about">About</Link>
-              </a>
-              <a className="navbar-item">
-                <Link to="/contact">Contact</Link>
-              </a>
               <a className="navbar-item">
                 <Link to="/home">Home</Link>
               </a>
@@ -45,116 +29,135 @@ export default function NavAuth(props) {
           </div>
         </>
       )
-      break
-    case '/contact':
-      navLanding = (
-        <>
-          <div id="navbarBasicExample " className="navbar-menu is-size-3">
-            <div className="navbar-end">
-              <a className="navbar-item">
-                <Link to="/about">About</Link>
-              </a>
-              <a className="navbar-item">
-                <Link to="/profile">Profile</Link>
-              </a>
-              <a className="navbar-item">
-                <Link to="/home">Home</Link>
-              </a>
-            </div>
-          </div>
-        </>
-      )
-      break
-    case '/about':
-      navLanding = (
-        <>
-          <div id="navbarBasicExample " className="navbar-menu is-size-3">
-            <div className="navbar-end">
-              <a className="navbar-item">
-                <Link to="/contact">Contact</Link>
-              </a>
-              <a className="navbar-item">
-                <Link to="/profile">Profile</Link>
-              </a>
-              <a className="navbar-item">
-                <Link to="/home">Home</Link>
-              </a>
-            </div>
-          </div>
-        </>
-      )
-      break
-    case '/listItem':
-      navLanding = (
-        <>
-          <div id="navbarBasicExample " className="navbar-menu is-size-3">
-            <div className="navbar-end">
-              <a className="navbar-item">
-                <Link to="/about">About</Link>
-              </a>
-              <a className="navbar-item">
-                <Link to="/contact">Contact</Link>
-              </a>
-              <a className="navbar-item">
-                <Link to="/profile">Profile</Link>
-              </a>
-              <a className="navbar-item">
-                <Link to="/home">Home</Link>
-              </a>
-            </div>
-          </div>
-        </>
-      )
-      break
-    case '/listings':
-      navLanding = (
-        <>
-          <div id="navbarBasicExample " className="navbar-menu is-size-3">
-            <div className="navbar-end">
-              <a className="navbar-item">
-                <Link to="/about">About</Link>
-              </a>
-              <a className="navbar-item">
-                <Link to="/contact">Contact</Link>
-              </a>
-              <a className="navbar-item">
-                <Link to="/profile">Profile</Link>
-              </a>
-              <a className="navbar-item">
-                <Link to="/home">Home</Link>
-              </a>
-            </div>
-          </div>
-        </>
-      )
-      break
-    default:
-      navLanding = (
-        <>
-          <div id="navbarBasicExample " className="navbar-menu is-size-3">
-            <div className="navbar-end">
-              <a className="navbar-item">
-                <Link to="/about">About</Link>
-              </a>
-              <a className="navbar-item">
-                <Link to="/contact">Contact</Link>
-              </a>
-              <a className="navbar-item">
-                <Link to="/profile">Profile</Link>
-              </a>
-              <a className="navbar-item">
-                <Link to="/home">Home</Link>
-              </a>
-            </div>
-          </div>
-        </>
-      )
+    //   break
+    // case '/profile':
+    //   navLanding = (
+    //     <>
+    //       <div id="navbarBasicExample " className="navbar-menu is-size-3">
+    //         <div className="navbar-end">
+    //           <a className="navbar-item">
+    //             <Link to="/about">About</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/contact">Contact</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/home">Home</Link>
+    //           </a>
+    //         </div>
+    //       </div>
+    //     </>
+    //   )
+    //   break
+    // case '/contact':
+    //   navLanding = (
+    //     <>
+    //       <div id="navbarBasicExample " className="navbar-menu is-size-3">
+    //         <div className="navbar-end">
+    //           <a className="navbar-item">
+    //             <Link to="/about">About</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/profile">Profile</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/home">Home</Link>
+    //           </a>
+    //         </div>
+    //       </div>
+    //     </>
+    //   )
+    //   break
+    // case '/about':
+    //   navLanding = (
+    //     <>
+    //       <div id="navbarBasicExample " className="navbar-menu is-size-3">
+    //         <div className="navbar-end">
+    //           <a className="navbar-item">
+    //             <Link to="/contact">Contact</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/profile">Profile</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/home">Home</Link>
+    //           </a>
+    //         </div>
+    //       </div>
+    //     </>
+    //   )
+    //   break
+    // case '/listItem':
+    //   navLanding = (
+    //     <>
+    //       <div id="navbarBasicExample " className="navbar-menu is-size-3">
+    //         <div className="navbar-end">
+    //           <a className="navbar-item">
+    //             <Link to="/about">About</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/contact">Contact</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/profile">Profile</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/home">Home</Link>
+    //           </a>
+    //         </div>
+    //       </div>
+    //     </>
+    //   )
+    //   break
+    // case '/listings':
+    //   navLanding = (
+    //     <>
+    //       <div id="navbarBasicExample " className="navbar-menu is-size-3">
+    //         <div className="navbar-end">
+    //           <a className="navbar-item">
+    //             <Link to="/about">About</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/contact">Contact</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/profile">Profile</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/home">Home</Link>
+    //           </a>
+    //         </div>
+    //       </div>
+    //     </>
+    //   )
+    //   break
+    // default:
+    //   navLanding = (
+    //     <>
+    //       <div id="navbarBasicExample " className="navbar-menu is-size-3">
+    //         <div className="navbar-end">
+    //           <a className="navbar-item">
+    //             <Link to="/about">About</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/contact">Contact</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/profile">Profile</Link>
+    //           </a>
+    //           <a className="navbar-item">
+    //             <Link to="/home">Home</Link>
+    //           </a>
+    //         </div>
+    //       </div>
+    //     </>
+    //   )
   }
 
   return (
     <>
       <IfAuthenticated>
-        <Link to="/home">{navLanding}</Link>
+        {navLanding}
         <div className="navbar-item">
           <div className="buttons">
             <a className="button is-primary is-light">
