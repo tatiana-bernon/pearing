1d30957c89646105a8ec3d94eba5bc1eb9390d14
Andras Balint <anoctus@protonmail.com>
2020-09-28 15:07:46 +1300

    work in progress - completed pearings

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import PersonalInfo from './PersonalInfo'
 import PersonalListing from './PersonalListing'
@@ -10,14 +9,6 @@ function Profile () {
       <PersonalInfo />
       <hr/>
       <PersonalListing />
-      <Link to='/addform'>
-        <button
-          type="button"
-          className="button is-primary"
-          data-testid="submitButton">
-            Create new pearing invitation!
-        </button>
-      </Link>
     </>
   )
 }
