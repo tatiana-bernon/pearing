0990b1d102ae82fe792541d7f38cc6d1d6d818fd
Bhavisha Das <bhivxx@gmail.com>
2020-09-29 09:49:32 +1300

    fixed forms

--- client/components/AddForm.jsx
+++ client/components/AddForm.jsx
@@ -4,7 +4,7 @@ import { getSubjects, addNewListing } from '../api'
 
 import { UserContext } from './UserContext'
 
-function AddForm () {
+function AddForm() {
   const [user] = useContext(UserContext)
   const [title, setTitle] = useState('')
   const [subject, setSubject] = useState('')
@@ -24,7 +24,7 @@ function AddForm () {
       })
   }, [])
 
-  function handleSubmit (e) {
+  function handleSubmit(e) {
     e.preventDefault()
     if (user.id) {
       const newListing = {
@@ -59,48 +59,56 @@ function AddForm () {
       <div>
         <br></br>
         <div>
-          <div className="has-text-primary mt-5 has-text-centered is-size-2 mb-2">
-            <h1>Create a new pearing invitation!</h1>
-          </div>
-          <form onSubmit={handleSubmit}>
+          <div className="container mx-6">
+            <div className="box">
+              <div className="columns is-centered is-5-tablet is-4-desktop is-3-widescreen">
+                <div className="field mt-5 mb-5">
+                  <div className="has-text-primary mt-5 has-text-centered is-size-2 mb-2">
+                    <h1>Create a new pearing invitation!</h1>
+                  </div>
+                  <form onSubmit={handleSubmit}>
 
-            {/* <input className="input is-normal" type="text"
+                    {/* <input className="input is-normal" type="text"
               placeholder="Pear level 1-10"
               name="pearLevel"
               value={ pearLevel } onChange={event => setpearLevel(event.target.value)}/> */}
-            <div className="has-text-centered select is-primary mb-5">
-              <select
-                className="select"
-                name="subjects"
-                id="subject"
-                onChange={event => setSubject(event.target.value)}>
-                <option>Select your subject</option>
-                {subjects.map(subject => (
-                  <option key={subject.id} value={subject.id}>{subject.subject}</option>
-                ))}
-              </select>
-            </div>
-            <div className="is-centered mb-5">
-              <h3 className="mb-2 label"> Title </h3>
-              <input className="input is-normal is-primary" type="text"
-                placeholder="Enter your title"
-                name="title"
-                value={title} onChange={event => setTitle(event.target.value)} />
-            </div>
+                    <div className="has-text-centered select is-primary mb-5">
+                      <select
+                        className="select"
+                        name="subjects"
+                        id="subject"
+                        onChange={event => setSubject(event.target.value)}>
+                        <option>Select your subject</option>
+                        {subjects.map(subject => (
+                          <option key={subject.id} value={subject.id}>{subject.subject}</option>
+                        ))}
+                      </select>
+                    </div>
+                    <div className="is-centered mb-5">
+                      <h3 className="mb-2 label"> Title </h3>
+                      <input className="input is-normal is-primary" type="text"
+                        placeholder="Enter your title"
+                        name="title"
+                        value={title} onChange={event => setTitle(event.target.value)} />
+                    </div>
 
-            <h5 h3 className="mb-2 label">Description</h5>
-            <textarea className="textarea is-normal is-primary"
-              placeholder="Enter your description"
-              name="description"
-              value={description} onChange={event => setDescription(event.target.value)} />
-            <br></br>
-            <div className= "container has-text-centered">
-              <button className="button is-medium is-primary mx-4 mt-6 mb-6 has-text-weight-medium is-centered">Submit</button>
-              <Link to='/home'>
-                <button className="button is-medium is-primary is-outlined mx-4 mt-6 mb-6 has-text-weight-medium is-centered">Back</button>
-              </Link>
+                    <h5 h3 className="mb-2 label">Description</h5>
+                    <textarea className="textarea is-normal is-primary"
+                      placeholder="Enter your description"
+                      name="description"
+                      value={description} onChange={event => setDescription(event.target.value)} />
+                    <br></br>
+                    <div className="container has-text-centered">
+                      <button className="button is-medium is-primary mx-4 mt-6 mb-6 has-text-weight-medium is-centered">Submit</button>
+                      <Link to='/home'>
+                        <button className="button is-medium is-primary is-outlined mx-4 mt-6 mb-6 has-text-weight-medium is-centered">Back</button>
+                      </Link>
+                    </div>
+                  </form>
+                </div>
+              </div>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </>
