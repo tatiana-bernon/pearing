29e38eb1b8c77b224da2659fd309df8529deb126
Bhavisha Das <bhivxx@gmail.com>
2020-09-28 14:13:05 +1300

    contact page style

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -1,70 +1,64 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
 
-function Contact () {
+function Contact() {
   return (
     <>
-      <h1 className="has-text-primary is-size-1 has-text-centered mx-6 mt-6 mb-6"> Send us a message! </h1>
-      <div className="field">
-        <label className="label">Name</label>
-        <div className="control">
-          <input className="input is-primary" type="text" placeholder="Primary input" />
-        </div>
-      </div>
+      <div className="container mx-6">
+        <div className="columns is-5-tablet is-4-desktop is-3-widescreen">
+          <div className="field mt-5 mb-5">
+            <h1 className="has-text-primary is-size-1 has-text-centered mx-6 mt-6 mb-6"> Send us a message! </h1>
+            <div className="field">
+              <label className="label">Name</label>
+              <div className="control">
+                <input className="input is-primary" type="text" placeholder="Name" />
+              </div>
+            </div>
 
-      <div className="field">
-        <label className="label">Username</label>
-        <div className="control has-icons-left has-icons-right">
-          <input className="input is-primary" type="text" placeholder="Primary input" />
-          <span className="icon is-small is-left">
-            <i className="fas fa-user"></i>
-          </span>
-          <span className="icon is-small is-right">
-            <i className="fas fa-check"></i>
-          </span>
-        </div>
-        {/* <p className="help is-success">This username is available</p> */}
-      </div>
+            <div className="field">
+              <label className="label">Username</label>
+              <div className="control has-icons-left has-icons-right">
+                <input className="input is-primary" type="text" placeholder="Username" />
+                <span className="icon is-small is-left">
+                  <i className="fas fa-user"></i>
+                </span>
+                <span className="icon is-small is-right">
+                  <i className="fas fa-check"></i>
+                </span>
+              </div>
+              {/* <p className="help is-success">This username is available</p> */}
+            </div>
 
-      <div className="field">
-        <label className="label">Email</label>
-        <div className="control has-icons-left has-icons-right">
-          <input className="input is-primary" type="text" placeholder="Primary input" />
-          <span className="icon is-small is-left">
-            <i className="fas fa-envelope"></i>
-          </span>
-          <span className="icon is-small is-right">
-            <i className="fas fa-exclamation-triangle"></i>
-          </span>
-        </div>
-        {/* <p className="help is-danger">This email is invalid</p> */}
-      </div>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control has-icons-left has-icons-right">
+                <input className="input is-primary" type="text" placeholder="Email" />
+                <span className="icon is-small is-left">
+                  <i className="fas fa-envelope"></i>
+                </span>
+                <span className="icon is-small is-right">
+                  <i className="fas fa-exclamation-triangle"></i>
+                </span>
+              </div>
+              {/* <p className="help is-danger">This email is invalid</p> */}
+            </div>
 
-      <div className="field">
-        <label className="label">Subject</label>
-        <div className="control">
-          <div className="select">
-            <select>
-              <option>Select dropdown</option>
-              <option>With options</option>
-            </select>
+            <div className="field">
+              <label className="label">Message</label>
+              <div className="control">
+                <textarea className="textarea is-primary" placeholder="Please enter your message"></textarea>
+              </div>
+            </div>
+
+            <div className="field is-grouped">
+              <div className="control">
+                <button className="button is-primary">Submit</button>
+              </div>
+              <div className="control">
+                <button className="button is-primary is-outlined">Cancel</button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label">Message</label>
-        <div className="control">
-          <textarea className="textarea is-primary" placeholder="Primary textarea"></textarea>
-        </div>
-      </div>
-
-      <div className="field is-grouped">
-        <div className="control">
-          <button className="button is-primary">Submit</button>
-        </div>
-        <div className="control">
-          <button className="button is-primary is-outlined">Cancel</button>
         </div>
       </div>
     </>
