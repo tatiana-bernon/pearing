13513f243a8c35be0fea39552f63e324f5c8d0b9
Daryl-creator <darylchen.w.h@gmail.com>
2020-09-29 16:48:34 +1300

    add link to home

--- client/components/NavUnauth.jsx
+++ client/components/NavUnauth.jsx
@@ -7,6 +7,7 @@ export default function NavUnauth () {
       <div>
         <div id="navbarBasicExample" className="navbar-menu is-size-3">
           <div className="navbar-end">
+            <Link className="navbar-item" to="/">Home</Link>
             <Link className="navbar-item" to="/about">About</Link>
             <Link className="navbar-item" to="/contact">Contact</Link>
           </div>
