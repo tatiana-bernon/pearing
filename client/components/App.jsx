15ea6cfac5ce5000ef6b7cf35e7834cdd35cfea7
Tatiana Bernon <42634827+tatiana-bernon@users.noreply.github.com>
2020-09-29 13:46:29 +1300

    logo change

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -21,7 +21,7 @@ import About from './About'
 // import PersonalInfo from './PersonalInfo'
 import Contact from './Contact'
 
-function App() {
+function App () {
   const [, setUser] = useContext(UserContext)
 
   useEffect(() => {
@@ -36,7 +36,9 @@ function App() {
         <div>
           <div>
             <div className="navbar is-primary has-text-white" role="navigation" aria-label="main navigation">
-              <img src="../images/pearinglogo.png" width="155" height="100" />
+              <div className= " mx-2 mt-2 has-text-centered">
+                <img src="../images/pearinglogo.png" width="200" height="50" />
+              </div>
               <div className="navbar-brand navbar-end">
                 <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" />
                 <span aria-hidden="true"></span>
