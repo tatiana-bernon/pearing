fcd511c7edcb8e31876e4fa6a5a0cac3484da706
taty500summers <tatiana.bernon@gmail.com>
2020-09-29 13:41:17 +1300

    Add final pics on slideshow

--- client/components/Slideshow.jsx
+++ client/components/Slideshow.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Slide } from 'react-slideshow-image';
 
 const slideImages = [
-  '../images/slideshow/4.JPG',
-  '../images/slideshow/31.JPG',
-  '../images/slideshow/25.JPG',
-  '../images/slideshow/18..JPG',
+  '../images/cropped_0002_13.png',
+  '../images/cropped_0004_25.png',
+  '../images/cropped_0001_12.png',
+  '../images/cropped_0006_31.png',
+  '../images/cropped_0003_15.png',
+  '../images/cropped_0007_32.png'
+  
 ];
 
 const Slideshow = () => {
@@ -36,6 +39,18 @@ const Slideshow = () => {
           <div className="each-slide">
             <div style={{'backgroundImage': `url(${slideImages[3]})`}}>
               {/* <span>Slide 3</span> */}
+            </div>
+          </div>
+
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[4]})`}}>
+              {/* <span>Slide 1</span> */}
+            </div>
+          </div>
+
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[5]})`}}>
+              {/* <span>Slide 1</span> */}
             </div>
           </div>
 
