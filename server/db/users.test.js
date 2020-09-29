85affe8df71d1db13bd65fe49e668cbfcd0a37de
Andras Balint <anoctus@protonmail.com>
2020-09-29 15:13:31 +1300

    5 users tests

--- server/db/users.test.js
+++ server/db/users.test.js
@@ -22,3 +22,31 @@ test('createUser creates a new user', () => {
       return expect(user).toEqual([5])
     })
 })
+
+test('userExists returns true if user exists', () => {
+  return users.userExists('andras', testDb)
+    .then(bool => {
+      return expect(bool).toBe(true)
+    })
+})
+
+test('userExists returns false if user does not exist', () => {
+  return users.userExists('notandras', testDb)
+    .then(bool => {
+      return expect(bool).toBe(false)
+    })
+})
+
+test('getUserById returns the correct user', () => {
+  return users.getUserById(4, testDb)
+    .then(user => {
+      return expect(user.username).toBe('andras')
+    })
+})
+
+test('getUserByName returns the correct user', () => {
+  return users.getUserByName('andras', testDb)
+    .then(user => {
+      return expect(user.id).toBe(4)
+    })
+})
