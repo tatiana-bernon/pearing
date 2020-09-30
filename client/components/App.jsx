156bcd6e7f9bff0873820033b49bf1c3f7e624f0
taty500summers <tatiana.bernon@gmail.com>
2020-09-30 21:53:18 +1300

    fix slideshow

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -20,7 +20,7 @@ import About from './About'
 import Contact from './Contact'
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
 
-function App () {
+function App() {
   const [, setUser] = useContext(UserContext)
 
   useEffect(() => {
@@ -53,41 +53,43 @@ function App () {
               </IfNotAuthenticated>
             </nav>
           </div>
-          <div>
-            <Route exact path='/' component={Welcome} />
-          </div>
-          <div>
-            <Route path="/register" component={Register} />
-          </div>
-          <div>
-            <Route path="/signin" component={SignIn} />
-          </div>
-          <div>
-            <Route path="/subjects" component={Subjects} />
-          </div>
-          <div>
-            <Route exact path="/profile" component={Profile} />
-          </div>
-          <div>
-            <Route path="/profile/edit" component={PersonalInfoEdit} />
-          </div>
-          <div>
-            <Route path="/pearings" component={Pearings} />
-          </div>
-          <div>
-            <Route path="/home" component={Home} />
-          </div>
-          <div>
-            <Route path="/listings/:id" component={ListItem} />
-          </div>
-          <div>
-            <Route path="/addform" component={AddForm} />
-          </div>
-          <div>
-            <Route path="/about" component={About} />
-          </div>
-          <div>
-            <Route path="/contact" component={Contact} />
+          <div className="container is-fullhd">            
+            <div>
+              <Route exact path='/' component={Welcome} />
+            </div>
+            <div>
+              <Route path="/register" component={Register} />
+            </div>
+            <div>
+              <Route path="/signin" component={SignIn} />
+            </div>
+            <div>
+              <Route path="/subjects" component={Subjects} />
+            </div>
+            <div>
+              <Route exact path="/profile" component={Profile} />
+            </div>
+            <div>
+              <Route path="/profile/edit" component={PersonalInfoEdit} />
+            </div>
+            <div>
+              <Route path="/pearings" component={Pearings} />
+            </div>
+            <div>
+              <Route path="/home" component={Home} />
+            </div>
+            <div>
+              <Route path="/listings/:id" component={ListItem} />
+            </div>
+            <div>
+              <Route path="/addform" component={AddForm} />
+            </div>
+            <div>
+              <Route path="/about" component={About} />
+            </div>
+            <div>
+              <Route path="/contact" component={Contact} />
+            </div>
           </div>
         </div>
       </main>
--- client/components/Slideshow.jsx
+++ client/components/Slideshow.jsx
@@ -8,56 +8,54 @@ const slideImages = [
   '../images/cropped_0006_31.png',
   '../images/cropped_0003_15.png',
   '../images/cropped_0007_32.png'
-  
+
 ];
 
 const Slideshow = () => {
 
-    return (
-  
-      
-        <Slide easing="ease">
+  return (
+    <Slide easing="ease">
 
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[3]})`}}>
-              {/* <span>Slide 1</span> */}
-            </div>
-          </div>
+      <div className="each-slide">
+        <div style={{ 'backgroundImage': `url(${slideImages[3]})` }}>
+          {/* <span>Slide 1</span> */}
+        </div>
+      </div>
 
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-              {/* <span>Slide 2</span> */}
-            </div>
-          </div>
+      <div className="each-slide">
+        <div style={{ 'backgroundImage': `url(${slideImages[1]})` }}>
+          {/* <span>Slide 2</span> */}
+        </div>
+      </div>
 
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-              {/* <span>Slide 3</span> */}
-            </div>
-          </div>
+      <div className="each-slide">
+        <div style={{ 'backgroundImage': `url(${slideImages[2]})` }}>
+          {/* <span>Slide 3</span> */}
+        </div>
+      </div>
 
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-              {/* <span>Slide 3</span> */}
-            </div>
-          </div>
+      <div className="each-slide">
+        <div style={{ 'backgroundImage': `url(${slideImages[0]})` }}>
+          {/* <span>Slide 3</span> */}
+        </div>
+      </div>
 
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[4]})`}}>
-              {/* <span>Slide 1</span> */}
-            </div>
-          </div>
+      <div className="each-slide">
+        <div style={{ 'backgroundImage': `url(${slideImages[4]})` }}>
+          {/* <span>Slide 1</span> */}
+        </div>
+      </div>
 
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[5]})`}}>
-              {/* <span>Slide 1</span> */}
-            </div>
-          </div>
+      <div className="each-slide">
+        <div style={{ 'backgroundImage': `url(${slideImages[5]})` }}>
+          {/* <span>Slide 1</span> */}
+        </div>
+      </div>
 
-        </Slide>
-      
-      
-    )
+    </Slide>
+
+
+  )
 };
 
 export default Slideshow
